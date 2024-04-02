a56f761130e2c404a68924066c167cc46ce52e8c
breeze-wink <1667535930@qq.com>
2024-04-03 01:49:17 +0800

    version 0.0.1

---
+++ include/add.h
@@ -0,0 +1,19 @@
+#ifndef ADD_H
+#define ADD_H
+
+#pragma once
+
+namespace breeze
+{
+    class add
+    {
+    public:
+        add();
+        ~add();
+        static void add_num(int &a, int &b);
+    private:
+
+    };
+}
+
+#endif
---
+++ src/add.cpp
@@ -0,0 +1,17 @@
+#include "add.h"
+using namespace breeze;
+
+add::add()
+{
+
+}
+
+add::~add()
+{
+
+}
+
+void add::add_num(int &a, int &b)
+{
+    a = a + b;
+}
